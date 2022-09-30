c4eb7c31fec10d8ae37dde3f7e24e17ac7509787
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-09-30 22:31:54 +0600

    Modify

--- app.js
+++ app.js
@@ -4,10 +4,21 @@
     increment or decrement; == i++
 */
 
-//Example - 01
-for(let i = 0; i<5; i++){
-    // document.write(i);
-    // document.write("<br>");
-    document.write(i + "<br>"); //Example Concat
+// //Example - 01
+// for(let i = 0; i<5; i++){
+//     // document.write(i);
+//     // document.write("<br>");
+//     document.write(i + "<br>"); //Example Concat
+// }
+//     document.write("<br> Rest of the code");
+
+
+//Example - 02  
+var i=0;
+for(; i<10; i++){
+    document.write(i + '<br>')
 }
     document.write("<br> Rest of the code");
+
+
+
