071b8d18cf273f40855d9851919536b84c9eeba0
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-09-30 22:07:03 +0600

    For Loop

---
+++ app.js
@@ -0,0 +1,12 @@
+/* 
+    initialization;         == i=0
+    test Condition;         == i<=5
+    increment or decrement; == i++
+*/
+
+//Example - 01
+for(let i = 0; i<5; i++){
+    // document.write(i);
+    // document.write("<br>");
+    document.write(i + "<br>"); //Example Concat
+}
